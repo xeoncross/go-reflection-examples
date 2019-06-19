0ebac6a50d0378063417716cf6daf3a9986e2ff6
xeoncross <david@xeoncross.com>
2019-06-19 15:32:44 -0500

    fixed clone example

--- clone/clone_test.go
+++ clone/clone_test.go
@@ -6,20 +6,20 @@ import (
 	"testing"
 )
 
+// Clone a struct knowing it's type
+
 type A struct {
 	S string
 }
 
 // https://play.golang.org/p/XHaffI2jAV9
-func cloneValue(v reflect.Value) reflect.Value {
-	aType := reflect.TypeOf(v)
-
+func cloneValue(t reflect.Type) reflect.Value {
 	// If pointer, reduce to value
-	if aType.Kind() == reflect.Ptr {
-		aType = aType.Elem()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
 	}
 
-	return reflect.New(aType)
+	return reflect.New(t)
 }
 
 func TestClone(t *testing.T) {
@@ -32,7 +32,7 @@ func TestClone(t *testing.T) {
 
 	a := &A{}
 
-	newA := cloneValue(reflect.ValueOf(a)).Interface().(*A)
+	newA := cloneValue(reflect.TypeOf(a)).Interface().(*A)
 
 	a.S = "A"
 	newA.S = "B"
@@ -40,9 +40,7 @@ func TestClone(t *testing.T) {
 	fmt.Printf("%#v\n", a)
 	fmt.Printf("%#v\n", newA)
 
-	// //
-	// if fmt.Sprintf("%T", a) != fmt.Sprintf("%T", newA) {
-	// 	t.Errorf("%T != %T", a, newA)
-	// }
-
+	if fmt.Sprintf("%T", a) != fmt.Sprintf("%T", newA) {
+		t.Errorf("%T != %T", a, newA)
+	}
 }
