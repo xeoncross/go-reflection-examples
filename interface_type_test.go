2bb778d7f993cecc441dfdd7a65d92d656abae86
xeoncross <david@xeoncross.com>
2019-06-27 14:26:39 -0500

    Added example of getting the reflect.Type of an interface

---
+++ interface_type_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"reflect"
+	"testing"
+)
+
+// The example belows shows how to get the reflect.Type of an interface.
+// Also included is an example of checking that a reflect.Type matches a known
+// interface (context.Context)
+//
+// Thanks to Petar Maymounkov for this trick
+// https://groups.google.com/forum/#!topic/golang-nuts/qgJy_H2GysY
+
+func TestInterface(t *testing.T) {
+
+	// First, create a nil instance of the interface
+	var interfaceInstance interface{} = (*context.Context)(nil)
+
+	// The reflect.Type we get from our actual codebase
+	// Here we just make one up
+	mytype := reflect.TypeOf(context.TODO())
+
+	if !mytype.Implements(reflect.TypeOf(interfaceInstance).Elem()) {
+		t.Error("Type does not implement context.Context")
+	}
+
+	if !isContext(mytype) {
+		t.Error("Type does not implement context.Context")
+	}
+
+}
+
+// TypeOf trick found at
+func isContext(r reflect.Type) bool {
+	return r.Implements(reflect.TypeOf((*context.Context)(nil)).Elem())
+}
